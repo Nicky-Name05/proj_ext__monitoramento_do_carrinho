d8094363b76526a4b601b3873b7ff19a7de82175
Nicky-Name05 <nicoleafvt@gmail.com>
2025-11-06 10:36:37 -0300

    Implement ESP-NOW data reception and handling
    Added ESP-NOW functionality to receive data from sender boards and updated structures accordingly.

--- webserver_receiver.ino
+++ webserver_receiver.ino
@@ -1,16 +1,63 @@
-
+#include "FS.h"
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
 #include <AsyncTCP.h>
 #include "LittleFS.h"
+#include <esp_now.h>
+// Must match the senders' structure
+typedef struct contador {
+    int id; // must be unique for each sender board
+    bool stt_carrinho;
+} contador;
+
+// Create a contador called myData
+contador myData;
+// Create a structure to hold the readings from each board
+contador ponto1;
+contador ponto2;
+
+
+// Create an array with all the structures
+contador boardsStruct[3] = {ponto1, ponto2};
+
 
 AsyncWebServer server(80);
+AsyncEventSource events("/events");
 
-const char* ssid = "THE_SSID";
-const char* password = "THE_PASSWORD";
+const char* ssid = "ValenteTakeda";
+const char* password = "ackmakVT96!";
+
+// callback function that will be executed when data is received
+void OnDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len) {
+  char macStr[18];
+  Serial.print("Packet received from: ");
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.println(macStr);
+  memcpy(&myData, incomingData, sizeof(myData));
+  Serial.printf("Board ID %u: %u bytes\n", myData.id, len);
+  // Update the structures with the new incoming data
+  boardsStruct[myData.id-1].stt_carrinho = myData.stt_carrinho;
+  Serial.printf("status da passagem do carrinho: %b \n", boardsStruct[myData.id-1].stt_carrinho);
+  Serial.println();
+}
  
 void setup() {
   Serial.begin(115200);
+  
+  //define o esp como station pro esp-now e como soft ap para servir como servidor do site
+  WiFi.mode(WIFI_AP_STA);
+
+  //inicializa o esp-now
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info
+  esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
+
   // Connect to Wi-Fi network with SSID and password
   Serial.print("Connecting to ");
   Serial.println(ssid);
@@ -24,19 +71,28 @@ void setup() {
   Serial.println("WiFi connected.");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-  server.begin();
-  
+  // inicia o littlefs
   initLittleFS();
     // Web Server Root URL
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
       request->send(LittleFS, "/web.html", "text/html");
     });
-
+    
+  events.onConnect([](AsyncEventSourceClient *client){
+    if(client->lastId()){
+      Serial.printf("Client reconnected! Last message ID that it got is: %u\n", client->lastId());
+    }
+    // send event with message "hello!", id current millis
+    // and set reconnect delay to 1 second
+    client->send("hello!", NULL, millis(), 10000);
+  });
+  server.addHandler(&events);
+  server.begin();
 
 }
 
 
-// Initialize LittleFS
+// Função de iniciar o littlefs
 void initLittleFS() {
   if (!LittleFS.begin(true)) {
     Serial.println("An error has occurred while mounting LittleFS");
@@ -44,7 +100,7 @@ void initLittleFS() {
   Serial.println("LittleFS mounted successfully");
 }
 
-// Read File from LittleFS
+// Função de ler o file do littlefs
 String readFile(fs::FS &fs, const char * path){
   Serial.printf("Reading file: %s\r\n", path);
 
@@ -60,7 +116,7 @@ String readFile(fs::FS &fs, const char * path){
     break;     
   }
   return fileContent;
-}
+} // fim da função que ler o littlefs
 
  
 void loop() {
