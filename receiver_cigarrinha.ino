9b97ff9b8cf992f44c72c1bbeba09e9409cb9bc1
Nicky-Name05 <nicoleafvt@gmail.com>
2025-12-03 17:02:35 -0300

    Refactor data structures and update logic
    Refactor receiver_cigarrinha.ino to improve data handling and structure.

--- receiver_cigarrinha.ino
+++ receiver_cigarrinha.ino
@@ -1,6 +1,16 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
+#define TXD1 19
+#define RXD1 21
+
+int n = 2; // numero de cigarrinhas
+int parada; // variavel que diz qual foi a parada q o carrinho passou
+bool passagem = false; // variavel usada pra marcar q houve uma passagem 
+bool stt_servico = false; // variavel q diz se o carrinho ta em rota ou nn
+
+
+HardwareSerial mySerial(1);
 // Must match the senders' structure
 typedef struct contador {z
     int id; // must be unique for each sender board
@@ -15,26 +25,27 @@ contador ponto2;
 
 
 // Create an array with all the structures
-contador boardsStruct[2] = {ponto1, ponto2};
+contador paradas[n] = {ponto1, ponto2};
 
 void OnDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len) {
   memcpy(&myData, incomingData, sizeof(myData));
   Serial.printf("Parada  %u: %u bytes\n", myData.id, len);
-  // Update the structures with the new incoming data
-  boardsStruct[myData.id-1].stt_carrinho = myData.stt_carrinho;
-  paradas[myData.id-1] = myData.stt_carrinho;
-  Serial.print(paradas);
-  Serial.println();
-  paradas[myData.id-1]++;
+
+  paradas[myData.id].stt_carrinho = myData.stt_carrinho;// Update the structures with the new incoming data
+  
+  if (myData.id = 0 && paradas[0].stt_carrinho == true && stt_serviço == true){ // esse if marca se o carrinho esta indo pra doca
+    stt_servico = false; // se o carrinho estiver saindo marca como false
+  } else{
+    stt_servico = true; //se nn marca como true
+  }
+  Serial.println(paradas);
+  passagem = true;// marca q precisa mandar dados pro esp servidor
 }
 
-int n = 2; // numero de paradas monitoradas mais a entrada/saida
-int paradas [n] = {0, 0};
-
 void setup() {
-  // put your setup code here, to run once:
 
   Serial.begin(115200);
+  mySerial.begin(115200, SERIAL_8N1, RXD1, TXD1);  // UART setup
   
   //define o esp como station pro esp-now
   WiFi.mode(WIFI_STA);
@@ -49,4 +60,10 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
+  if (passagem == true){
+    mySerial.print(String(myData.id)); // manda qual parada o carrinho acabou de passar
+    mySerial.print(String(stt_servico)); // manda se o carrinho ainda esta em rota
+    passagem = false;
+  } 
+  delay(2);
 }
